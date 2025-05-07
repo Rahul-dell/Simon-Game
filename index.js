40f4cc4a72feabf5984d54da1ef2f34e4946d10e
rahul <ujjwalbijalwan589@gmail.com>
2025-05-07 09:16:41 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+$(".yellow").on("click", function(){
+
+    var audio = new Audio("assets/yellow.mp3");
+    audio.play();
+
+});
+$(".red").on("click", function(){
+
+    var audio = new Audio("assets/red.mp3");
+    audio.play();
+
+});
+$(".green").on("click", function(){
+
+    var audio = new Audio("assets/green.mp3");
+    audio.play();
+
+});
+$(".blue").on("click", function(){
+
+    var audio = new Audio("assets/blue.mp3");
+    audio.play();
+
+});
+function wrong(){
+
+    var audio = new Audio("assets/wrong.mp3");
+    audio.play();
+
+}
+function flash(){
+    $("")
+}
