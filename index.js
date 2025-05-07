fc92026d1e9a0d19a8021915278d97c4393daeb7
rahul <ujjwalbijalwan589@gmail.com>
2025-05-07 11:38:54 +0530

    Add more javascript for sounds and animations

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
 
-
-$(".yellow").on("click", function(){
-
+function yellow(){
     var audio = new Audio("assets/yellow.mp3");
-    audio.play();
-
-});
-$(".red").on("click", function(){
-
+        audio.play();}
+        function red(){
     var audio = new Audio("assets/red.mp3");
-    audio.play();
+        audio.play();}
+        function blue(){
+    var audio = new Audio("assets/blue.mp3");
+        audio.play();}
+        function green(){
+    var audio = new Audio("assets/green.mp3");
+        audio.play();}
+$(".yellow").on("click", function(){
+    yellow();
+    flash (this);
+   
+});
 
+$(".red").on("click", function(){
+    red();
+    flash(this);
+    
 });
 $(".green").on("click", function(){
 
-    var audio = new Audio("assets/green.mp3");
-    audio.play();
+
+    green();
+    flash(this);
 
 });
 $(".blue").on("click", function(){
-
-    var audio = new Audio("assets/blue.mp3");
-    audio.play();
+    blue();
+    flash(this);
 
 });
+function flash(arg){
+$(arg).addClass("flash");
+setTimeout(function(){$(arg).removeClass("flash");},100);
+}
 function wrong(){
 
     var audio = new Audio("assets/wrong.mp3");
     audio.play();
+    $("h1").text("Game Over Press Any Key to Restart");
+    $("body").addClass("wrong");
+    setTimeout(function (){$("body").removeClass("wrong");},500);
 
 }
-function flash(){
-    $("")
+
+function seek(arg)
+{
+    
+    $(arg).addClass("seek");
+    setTimeout(function(){$(arg).removeClass("seek");}, 200);
 }
+
